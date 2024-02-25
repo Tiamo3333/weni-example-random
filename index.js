a48b83683f3278b2d0658ea2e6cac78b2824adcd
Tiamo <1036976281@qq.com>
2024-02-25 23:26:00 +0800

    init commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function random(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function generateRandomWord() {
+    const vowels = 'aeiou';
+    const consonants = 'bcdfghjklmnpqrstvwxyz';
+    let word = '';
+    const length = Math.floor(Math.random() * (10 - 3 + 1)) + 3; // 可以根据需要调整单词长度范围
+
+    for (let i = 0; i < length; i++) {
+        if (i % 2 === 0) {
+            word += consonants.charAt(Math.floor(Math.random() * consonants.length));
+        } else {
+            word += vowels.charAt(Math.floor(Math.random() * vowels.length));
+        }
+    }
+
+    return word;
 }
 
-module.exports = random;
+// 示例用法
+const randomWord = generateRandomWord();
+console.log(randomWord);
